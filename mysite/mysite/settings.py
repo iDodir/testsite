39e01f1974b68a09aae0de5bd817fadb3dc280d7
dodir <mokronosovmw@gmail.com>
2022-03-23 23:29:45 +0500

    [upd] admin setup

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -117,5 +117,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import News
+
 
 # Create your views here.
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+    }
+    return render(request, template_name='news/index.html', context=context)
