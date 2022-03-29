ec6493b6445902ac3dc033c9e79efc02b5a70127
dodir <mokronosovmw@gmail.com>
2022-03-29 23:13:22 +0500

    [upd] setup templates

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
+    path('category/<int:category_id>/', views.get_category, name='category'),
 ]
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 
 # Create your views here.
@@ -14,3 +14,10 @@ def index(request):
         'title': 'Список новостей',
     }
     return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id: int):
+    news = News.objects.filter(category_id=category_id)
+    category = Category.objects.get(pk=category_id)
+    return render(request, template_name='news/category.html',
+                  context={'news': news, 'category': category})
