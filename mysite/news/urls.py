ecbd55664874e7415612a985b73df82d1d7329a9
dodir <mokronosovmw@gmail.com>
2022-04-03 23:47:29 +0500

    [upd] using classes as views

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('category/<int:category_id>/', views.get_category, name='category'),
-    path('news/<int:news_id>/', views.view_news, name='view_news'),
-    path('news/add-news/', views.add_news, name='add_news'),
+    # path('', views.index, name='home'),
+    path('', views.HomeNews.as_view(), name='home'),
+    # path('category/<int:category_id>/', views.get_category, name='category'),
+    path('category/<int:category_id>/', views.NewsByCategory.as_view(), name='category'),
+    # path('news/<int:news_id>/', views.view_news, name='view_news'),
+    path('news/<int:pk>/', views.ViewNews.as_view(), name='view_news'),
+    # path('news/add-news/', views.add_news, name='add_news'),
+    path('news/add-news/', views.CreateNews.as_view(), name='add_news'),
 ]
